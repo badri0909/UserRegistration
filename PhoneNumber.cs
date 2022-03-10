5b4d2a8eee492e13844abc2426f922dc1a7925a3
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-10 07:52:22 +0530

    add PhoneNumber.cs

---
+++ PhoneNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    public class PhoneNumber
+    {
+        public static string Regex_Pattern = "^[+91]{3}[' ']*[7-9]{1}[0-9]{9}$";
+        public bool validatePhoneNumber(string phonenumber)
+        {
+            return Regex.IsMatch(phonenumber, Regex_Pattern);
+        }
+    }
+}
