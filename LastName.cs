4f83e874b20e5b0749d5f4ed1cd4dbacccacd94a
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-10 07:49:04 +0530

    add LastName.cs

---
+++ LastName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    public class LastName
+    {
+        public static string Regex_Pattern = "^[A-Z]{1}[a-z]{3,}$";
+        public bool validateLastName(string lastname)
+        {
+            return Regex.IsMatch(lastname, Regex_Pattern);
+        }
+    }
+}
