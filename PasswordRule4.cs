2fe2a5e6ab144c875fa7f580150db97f1559f4d9
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-10 08:03:37 +0530

    add PasswordRule4.cs

---
+++ PasswordRule4.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    public class PasswordRule4
+    {
+        public static string Regex_Pattern = "(^[a-z A-Z 0-9])([^.,:;'!@#$%^&*_+=]).{8,}$";
+        public bool validatePasswordRule4(string passwordrule4)
+        {
+            return Regex.IsMatch(passwordrule4, Regex_Pattern);
+        }
+    }
+}
