7072eba9c605f481dee39b9ca839f2e0da36d772
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-10 07:53:55 +0530

    add PasswordRule1.cs

---
+++ PasswordRule1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    public class PasswordRule1
+    {
+        public static string Regex_Pattern = "^[a-z]{8,}$";
+        public bool validatePasswordRule1(string passwordrule1)
+        {
+            return Regex.IsMatch(passwordrule1, Regex_Pattern);
+        }
+    }
+}
