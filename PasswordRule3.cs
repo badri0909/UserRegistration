2211b1ee884c840875da356ba32c8d61d81a980a
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-10 08:02:08 +0530

    add PasswordRule3.cs

---
+++ PasswordRule3.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    public class PasswordRule3
+    {
+        public static string Regex_Pattern = "^[a-z A-Z 0-9]{8,}$";
+        public bool validatePasswordRule3(string passwordrule3)
+        {
+            return Regex.IsMatch(passwordrule3, Regex_Pattern);
+        }
+    }
+}
