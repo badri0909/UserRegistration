a15cfb7deeb49a1f8b8a5a28e7373587fe0d2b5a
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-16 11:18:35 +0530

    add ValidateUserEntry.cs

---
+++ ValidateUserEntry.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    class ValidateUserEntry
+    {
+        private string message;
+        UserRegistration userRegistration = new UserRegistration();
+        public ValidateUserEntry()
+        {
+        }
+        public ValidateUserEntry(string message)
+        {
+            this.message = message;
+        }
+        public string checkForFirstName()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Fails the Entry";
+        }
+        public string checkForLastName()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Fails the Entry";
+        }
+        public string checkForEmail()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Fails the Entry";
+        }
+        public string checkForMobile()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Fails the Entry";
+        }
+        public string checkForPassword()
+        {
+            if (message.Contains("Happy"))
+                return "Entry is successful";
+            else
+                return "Fails the Entry";
+        }
+    }
+}
