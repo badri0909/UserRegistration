cd0be65118d94051c63cd1d19847d08597eba1c6
badri0909 <singuveerabhadrarao@gmail.com>
2022-03-16 11:19:50 +0530

    add CustomException.cs

---
+++ CustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegxDemo
+{
+    public class CustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            INVALID_FIRSTNAME,
+            INVALID_LASTNAME,
+            INVALID_EMAIL,
+            INVALID_MOBILE_NUMBER,
+            INVALID_PASSWORD
+        }
+        private readonly ExceptionType type;
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
